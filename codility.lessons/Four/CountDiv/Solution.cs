2e726fa031e67cdbca8def630406ff0c8c801f09
horea80 <horea.hopartean@gmail.com>
2016-01-06 23:45:09 +0200

    added failing test for counting divisors

---
+++ codility.lessons/Four/CountDiv/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codility.Four.CountDiv
+{
+    public class Solution
+    {
+        public int solution(int A, int B, int K)
+        {
+            return 0;
+        }
+    }
+}
---
+++ codility.test/Four/CountDiv/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using codility.Four.CountDiv;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codility.Four.CountDiv.Tests
+{
+    [TestFixture()]
+    public class SolutionTests
+    {
+        [Test()]
+        public void solutionTest()
+        {
+            Solution s = new Solution();
+            Assert.AreEqual(3, s.solution(6, 11, 2));
+            
+        }
+    }
+}
