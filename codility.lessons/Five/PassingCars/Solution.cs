f2fd1cb5eb7a37a7c0c0edd0fb11f9d4995e2651
horea80 <horea.hopartean@gmail.com>
2016-01-09 23:07:51 +0200

    Added the car passing solution

---
+++ codility.lessons/Five/PassingCars/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace codility.Five.PassingCars
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int N = A.Length;
+            int eastBoundCount = 0;
+            int passingCount = 0;
+
+            for(int i = 0; i < N; i++)
+            {
+                if (A[i] == 1)
+                {
+                    passingCount += eastBoundCount;
+                }
+                else
+                {
+                    eastBoundCount++;
+                }
+                if (passingCount > 1000000000) return -1;
+            }
+            return passingCount;
+        }
+    }
+}
---
+++ codility.test/Five/PassingCars/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using codility.Five.PassingCars;
+
+namespace codility.Five.PassingCars.Tests
+{
+    [TestFixture()]
+    public class SolutionTests
+    {
+        [Test()]
+        public void exampleTest()
+        {
+            Solution s = new Solution();
+            Assert.AreEqual(5, s.solution(new int[] { 0, 1, 0, 1, 1 }));
+        }
+        [Test()]
+        public void givenAllEast()
+        {
+            Solution s = new Solution();
+            Assert.AreEqual(0, s.solution(new int[] { 0, 0, 0, 0, 0 }));
+        }
+        [Test()]
+        public void givenAllWest()
+        {
+            Solution s = new Solution();
+            Assert.AreEqual(0, s.solution(new int[] { 1, 1, 1, 1, 1 }));
+        }
+    }
+}
