2d5afe027e57a1a5455444c7dcf898e1bc842e25
horea80 <horea.hopartean@gmail.com>
2016-01-17 09:58:46 +0200

    Added failing test for Minimum Average Slice

---
+++ codility.test/Five/MinAvgSlice/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿using codility.Five.MinAvgSlice;
+using NUnit.Framework;
+
+namespace codility.test.Five.MinAvgSlice
+{
+    [TestFixture()]
+    public class SolutionTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Solution s = new Solution();
+            var sliceStart = s.solution(new [] {4, 2, 2, 5, 1, 5, 8});
+            Assert.AreEqual(1, sliceStart);
+        }
+    }
+}
