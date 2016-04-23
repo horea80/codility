964406bb01cff25c9e82cb6335dbe3c4c2cabbf1
horea80 <horea.hopartean@gmail.com>
2016-04-23 23:02:45 +0300

    Valid MaxProductOfThree solution

---
+++ codility.lessons/Six/MaxProductOfThree/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace codility.Six.MaxProductOfThree
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            Array.Sort(A);
+            var n = A.Length;
+
+            var maxOne = A[n - 1]*A[n - 2]*A[n - 3];
+            var maxTwo = A[0]*A[1]*A[n - 1];
+
+            return Math.Max(maxOne, maxTwo);
+        }
+    }
+}
---
+++ codility.test/Six/MaxProductOfThree/SolutionTests.cs
@@ -0,0 +1,36 @@
+﻿using codility.Six.MaxProductOfThree;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace codility.test.Six.MaxProductOfThree
+{
+    public class SolutionTests
+    {
+        [Test]
+        public void GivenExerciseSample_ThenSolveIt()
+        {
+            int[] A = new[] {-3, 1, 2, -2, 5, 6};
+            var sol = new Solution();
+
+            Assert.AreEqual(60, sol.solution(A));
+        }
+
+        [Test]
+        public void GivenAllNegatives()
+        {
+            int[] A = new[] { -3, -1000, -2, -2, -5, -6 };
+            var sol = new Solution();
+
+            Assert.AreEqual(-12, sol.solution(A));
+        }
+
+        [Test]
+        public void GivenOnePositive()
+        {
+            int[] A = new[] { -3, -1000, -2, -2, -5, 1 };
+            var sol = new Solution();
+
+            Assert.AreEqual(5000, sol.solution(A));
+        }
+    }
+}
